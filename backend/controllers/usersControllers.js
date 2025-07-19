86a1fbc5ccf6d592c37df3d9659a5c5f4aec1753
unknown <fasuarezd@gmail.com>
2025-07-19 14:03:17 -0600

    Fin de la sesion 2

---
+++ backend/controllers/usersControllers.js
@@ -0,0 +1,53 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/usersModels')
+
+const registrar = asyncHandler(async (req, res) => {
+
+    //Desestructurar el body
+    const { nombre, email, password } = req.body
+
+    //Verificar que nos pasen todos los datos
+    if (!nombre || !email || !password) {
+        res.status(400)
+        throw new Error('Faltan datos')
+    }
+
+    //Hacemos el HASH del password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Crear al usuario
+    const user = await User.create({
+        nombre,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            nombre: user.nombre,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('No se pudo crear el usuario')
+    }
+
+})
+
+const login = asyncHandler(async (req, res) => {
+
+})
+
+const misDatos = asyncHandler(async (req, res) => {
+
+})
+
+module.exports = {
+    registrar,
+    login,
+    misDatos
+}
